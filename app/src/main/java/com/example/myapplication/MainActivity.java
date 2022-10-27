ef8023db01442f66fe4b9e47c0dc46bf0b979cb6
Lucas Salvatori <lucas.salvatori@somoscoders.org>
2022-10-27 15:25:02 -0300

    Mi primer commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        miMetodo();
+    }
+
+    private void miMetodo(){
+        System.out.println("Hola");
+    }
+
+    public void mensaje(View v){
+//        mensaje por consola
+        System.out.println("Hola desde boton!!!");
+//        mensaje con un tiempo de duracion
+        Toast toast = Toast.makeText(this, "Hola desde boton", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0,0);
+        toast.setText("Cambio de texto del mensaje del boton");
+        toast.show();
+//        mensaje en el textView
+        final TextView helloTextView = (TextView) findViewById(R.id.textView);
+        helloTextView.setText("Hola Desde boton!!!");
+//        sistema de numeracion decimal 0 --> 9 (10 digitos)
+//        sistema de numeracion hexadecimal 0 --> 9, A,B,C,D,E,F (16 digitos)
+//        helloTextView.setTextColor(0xFF00f4b6);
+        helloTextView.setTextColor(getResources().getColor(R.color.purple_700));
+        helloTextView.setTextSize(36);
+    }
+}
